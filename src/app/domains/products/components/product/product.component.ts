264b0612b94a51e748da14603f799e7f508745c0
leruizr <leruizres1986@gmail.com>
2024-02-21 17:21:10 -0500

    se organiza el formato para el producto con el boton de agregar al carrito

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -8,6 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  img = 'https://picsum.photos/640/640?r=' + Math.random();
+  @Input({required: true}) img: string = '';
+  @Input({required: true}) price: number = 0;
+  @Input({required: true}) title: string = '';
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler() {
+    console.log('click from child');
+    this.addToCart.emit('hola este es un msg desde el hijo ' + this.title);
+  }
+  
 
 }
