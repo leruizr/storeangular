61f726d7130f159e074dab119a57a262cebf206a
leruizr <leruizres1986@gmail.com>
2024-03-10 16:43:19 -0500

    se crea el header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,15 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component'
+import { Product } from './../../../shared/models/product.model'
+import { HeaderComponent } from './../../../shared/components/header/header.component'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Producto 1',
+        price: 1000,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 2',
+        price: 1100,
+        image: 'https://picsum.photos/640/640?r=24',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 3',
+        price: 1200,
+        image: 'https://picsum.photos/640/640?r=25',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 4',
+        price: 1000,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 5',
+        price: 1100,
+        image: 'https://picsum.photos/640/640?r=24',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 6',
+        price: 1200,
+        image: 'https://picsum.photos/640/640?r=25',
+        creationAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
+
   fromChild(event: string) {
     console.log('estamos en el padre');
     console.log(event);
