41fabbc76870f53b153bdbcceec2f7fa1313bddb
leruizr <leruizres1986@gmail.com>
2024-03-14 16:08:59 -0500

    se implementan pipes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../../shared/models/product.model';
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [ UpperCasePipe, CurrencyPipe, DatePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
