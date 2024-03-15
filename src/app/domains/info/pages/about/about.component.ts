89466f151a87656e8ba7d41a00334edb4f7bd194
leruizr <leruizres1986@gmail.com>
2024-03-15 14:23:16 -0500

    se crea una directiva

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { CounterComponent } from "../../../shared/components/counter/counter.component";
+import { CounterComponent } from "@shared/components/counter/counter.component";
 import { CommonModule } from '@angular/common';
 import { WaveAudioComponent } from "../../../info/components/wave-audio/wave-audio.component";
+import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 @Component({
     selector: 'app-about',
     standalone: true,
     templateUrl: './about.component.html',
     styleUrl: './about.component.css',
-    imports: [CounterComponent, CommonModule, WaveAudioComponent]
+    imports: [CounterComponent, CommonModule, WaveAudioComponent, HighlightDirective]
 })
 export class AboutComponent {
   duration = signal(1000);
